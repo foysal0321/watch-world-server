31808e4b951ac2b7e7832816b3f630e2baf81e97
Foysal Hossain <foysalx4@gmail.com>
2022-11-24 20:02:05 +0600

    dress-watch-done

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
+//middle ware
 const app= express();
-app.use(express.json())
 app.use(cors());
+app.use(express.json())
+
+
+//mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pbaqirc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const dressColletion = client.db('watch-world').collection('dress')
+
+        app.get('/dress-watch', async (req,res)=>{
+            const query = {}
+            const result = await dressColletion.find(query).limit(1).toArray()
+            res.send(result)
+        })
+        app.get('/dress-watchs', async (req,res)=>{
+            const query = {}
+            const result = await dressColletion.find(query).toArray()
+            res.send(result)
+        })
+
+       
+        
+
+    }
+    finally{
+
+    }
+}
+ run().catch(err=>console.log(err))
+
 
 app.get('/', (req,res)=>{
     res.send('server is running..!')
