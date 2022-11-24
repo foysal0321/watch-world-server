391af3699f8e7f763d826ebbc7408e77b9d75253
Foysal Hossain <foysalx4@gmail.com>
2022-11-24 23:19:41 +0600

    all-seller-done

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const dressColletion = client.db('watch-world').collection('dress')
+        const sportColletion = client.db('watch-world').collection('sport-watch')
+        const woodColletion = client.db('watch-world').collection('wood-watch')
+        const buyerColletion = client.db('watch-world').collection('buyers')
+        const sellerColletion = client.db('watch-world').collection('seller')
 
+        //dress-watch
         app.get('/dress-watch', async (req,res)=>{
             const query = {}
             const result = await dressColletion.find(query).limit(1).toArray()
@@ -26,6 +31,55 @@ async function run(){
         app.get('/dress-watchs', async (req,res)=>{
             const query = {}
             const result = await dressColletion.find(query).toArray()
+            res.send(result)
+        })
+
+        //sport-watch
+        app.get('/sport-watch', async (req,res)=>{
+            const query = {}
+            const result = await sportColletion.find(query).limit(1).toArray()
+            res.send(result)
+        })
+        app.get('/sport-watchs', async (req,res)=>{
+            const query = {}
+            const result = await sportColletion.find(query).toArray()
+            res.send(result)
+        })
+
+        //wood-watch
+        app.get('/wood-watch', async (req,res)=>{
+            const query = {}
+            const result = await woodColletion.find(query).limit(1).toArray()
+            res.send(result)
+        })
+        app.get('/wood-watchs', async (req,res)=>{
+            const query = {}
+            const result = await woodColletion.find(query).toArray()
+            res.send(result)
+        })
+
+        //buyer
+        app.post('/buyers', async(req,res)=>{
+            const query = req.body;
+            const result = await buyerColletion.insertOne(query)
+            res.send(result)
+        })
+
+        app.get('/buyers', async(req,res)=>{
+            const query = {};
+            const result = await buyerColletion.find(query).toArray()
+            res.send(result)
+        })
+        //seller
+        app.post('/seller', async(req,res)=>{
+            const query = req.body;
+            const result = await sellerColletion.insertOne(query)
+            res.send(result)
+        })
+
+        app.get('/seller', async(req,res)=>{
+            const query = req.body;
+            const result = await sellerColletion.find(query).toArray()
             res.send(result)
         })
 
